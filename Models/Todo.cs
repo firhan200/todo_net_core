e4f51c47935e775b6a295904b5a8745769196909
firhan faisal <firhan@jmdigital.id>
2023-08-10 23:45:03 +0700

    first commit

---
+++ Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿namespace Todov2.Models
+{
+    public class Todo
+    {
+        public Todo(int? id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        public int? Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Todov2.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var todoRouter = app.MapGroup("/todo");
+
+todoRouter.MapGet("/", () =>
+{
+    List<string> todos = new List<string>();
+    todos.Add("Eat");
+    todos.Add("Sleep");
+    todos.Add("Repeat");
+    return Results.Ok(todos);
+});
+
+todoRouter.MapPost("/", (Todo todo) =>
+{
+    return todo;
+});
+
+todoRouter.MapPut("/{id}", (int id, Todo todo) =>
+{
+    todo.Id = id;
+    return todo;
+});
+
+todoRouter.MapDelete("/", () =>
+{
+    return "delete todo";
+});
+
+app.Run();
